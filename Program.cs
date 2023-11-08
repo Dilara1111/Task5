343bab7e4311f1da3b7bc7dfcdde648668cd4ff3
HuseynovaDilara <huseynova.8885@gmail.com>
2023-11-08 10:10:47 +0400

    Task # 5 added

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //        Tapşırıq:
+            //Student classınız olsun. int[] Grades propertysi olsun.
+            //Studentdən obyekt yaradıb grades-i set edəndə arrayin yalnız cüt ədədlərindən ibarət yeni bir array set olunsun.
+            //Yəni əgər mən
+            //Student st = new Student();
+            //st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }
+            //yazıramsa st.Grades { 2,4,6,8}-ə bərabər olsun//  
+            Student st = new Student();
+            st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Student
+    {
+        private int[] _grades;
+        public int[] Grades
+        {
+            get 
+            {               
+                return _grades;
+            }
+            set 
+            {
+                int counter = 0;
+                foreach (var item in value)
+                {
+                    if (item%2 == 0)
+                    {
+                        counter++;
+                    }
+                }
+                _grades = new int[counter];
+                for (int i = 0, j = 0; i < value.Length; i++)
+                {     
+                    if (value[i] % 2 == 0)
+                    {
+                        _grades[j++] = value[i];
+                    }
+                }
+            }
+           
+
+        }
+
+    }
+}
